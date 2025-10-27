a46922517345e6f22e6cffea0d6b5f6a6d820031
Mikhail Khorsun <horsunmv@bsb.by>
2025-10-27 13:26:23 +0300

    homework-5

--- homework_05/car.py
+++ homework_05/car.py
@@ -1,3 +1,16 @@
 """
 Создайте класс `Car`, наследник `Vehicle`
 """
+
+from homework_05.base import Vehicle
+from homework_05.engine import Engine
+
+
+class Car(Vehicle):
+  def __init__(self, weight=0, fuel=0, fuel_consumption=0):
+    super().__init__(weight=weight, fuel=fuel,
+                     fuel_consumption=fuel_consumption)
+    self.engine = None
+
+  def set_engine(self, engine: Engine):
+    self.engine = engine
--- homework_05/exceptions.py
+++ homework_05/exceptions.py
@@ -4,3 +4,18 @@
 - NotEnoughFuel
 - CargoOverload
 """
+
+
+class LowFuelError(Exception):
+  """Ошибка: недостаточно топлива для запуска двигателя."""
+  pass
+
+
+class NotEnoughFuel(Exception):
+  """Ошибка: недостаточно топлива для движения."""
+  pass
+
+
+class CargoOverload(Exception):
+  """Ошибка: перегруз транспортного средства."""
+  pass
